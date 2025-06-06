3b08736606fc57d5c8362e38b73f2c29e14e4503
SK-Saba-Tarannum <sabatarannum0303@gmail.com>
2025-06-06 20:09:22 +0530

    checking product page

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/product.controller');
 const { authenticate, authorizeRoles } = require('../middlewares/auth.middleware');
 
-router.post('/addproduct', authenticate, authorizeRoles, productController.addProduct);
+router.post('/addproduct', authenticate, authorizeRoles(['ADMIN']), productController.addProduct);
 router.get('/allproducts', productController.fetchProducts);
 router.get('/:id', productController.fetchProductById);
 
